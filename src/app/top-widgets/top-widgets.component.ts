955e7267dd1d4ff8cd038b8d663bb9ed81cd6737
Chimankpa Osuji <chimankpaosuji@gmail.com>
2023-07-06 17:14:55 +0100

    first commit

--- src/app/top-widgets/top-widgets.component.ts
+++ src/app/top-widgets/top-widgets.component.ts
@@ -5,14 +5,6 @@ import {
   faBoxes,
   faMoneyBill,
 } from '@fortawesome/free-solid-svg-icons';
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 @Component({
   selector: 'app-top-widgets',
